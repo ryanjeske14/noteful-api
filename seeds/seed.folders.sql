23da5a7d68e51cd2b6e42b15f4c46853f1f40c30
Ryan Jeske <ryanjeske@gmail.com>
2019-05-09 18:05:51 -0600

    initial commit

---
+++ seeds/seed.folders.sql
@@ -0,0 +1,5 @@
+INSERT INTO folders (name)
+VALUES
+    ('Folder 1'),
+    ('Folder 2'),
+    ('Folder 3')
---
+++ seeds/seed.notes.sql
@@ -0,0 +1,8 @@
+INSERT INTO notes (name, content, folder_id)
+VALUES
+    ('First note!', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 1),
+    ('Second note!', 'Possimus, voluptate? Necessitatibus, reiciendis?', 1),
+    ('Third note!', 'Est nostrum esse porro id quaerat.', 1),
+    ('Fourth note!', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 2),
+    ('Fifth note!', 'Possimus, voluptate? Necessitatibus, reiciendis?', 2),
+    ('Sixth note!', 'Est nostrum esse porro id quaerat.', 3)
