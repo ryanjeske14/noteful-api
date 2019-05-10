f63dc40b707cf55eb5429ee8d5521e11b8ab3cab
Ryan Jeske <ryanjeske@gmail.com>
2019-05-10 17:10:12 -0600

    updated config file

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   DB_URL:
-    process.env.DB_URL || "postgresql://dunder-mifflin:Office@localhost/noteful"
+    process.env.DATABASE_URL ||
+    "postgresql://dunder-mifflin:Office@localhost/noteful"
 };
